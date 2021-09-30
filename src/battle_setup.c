5a8b17102d10762994e1c5c5c416240c8b11aa5a
LucaBGt <32401333+LucaBGt@users.noreply.github.com>
2021-09-30 16:29:49 +0200

    + Changed Juntrick's Battle Theme

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -573,7 +573,7 @@ void BattleSetup_StartLegendaryBattle(void)
         CreateBattleStartTask(B_TRANSITION_GRID_SQUARES, MUS_VS_MEW);
         break;
     case SPECIES_JUNTRICK:
-        CreateBattleStartTask(B_TRANSITION_BLUR, MUS_ABANDONED_SHIP);
+        CreateBattleStartTask(B_TRANSITION_BLUR, MUS_RG_VS_WILD);
         break;
     }
 
